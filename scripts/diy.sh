3fa700b8b729720a2129fc3239cb8928433a4be4
tntsu37 <161291069+tntsu37@users.noreply.github.com>
2024-07-04 19:22:10 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,12 +2,3 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-
-#添加easymesh
-echo 'src-git luci-app-easymesh https://github.com/shuishihan/easymesh.git' >> feeds.conf.default
-#添加luci-app-adguardhome
-echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git' >> feeds.conf.default
-# istore
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default

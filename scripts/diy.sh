ae31b7c5be3e786096824fb809386fa7a323c52f
tntsu37 <161291069+tntsu37@users.noreply.github.com>
2025-02-14 08:28:06 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #更改默认地址为192.168.6.1
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 #删除冲突的软件包
 #rm -rf ./package/istore
